a248a2da7bcd808decf3ad1dd9b944f421a95649
Arne Van Raepenbusch <arne.vanraepenbusch@jstack.eu>
2018-03-26 11:24:24 +0200

    initial commit

---
+++ _exercises/0-hello-node.js
@@ -0,0 +1,25 @@
+/*
+
+    Welcome to the node workshop.
+
+    These exercises will help you getting comfortable with nodejs. First we will see a bit of nodejs native functionality
+    then we'll move on to servers, api's, and some frameworks that are useful.
+
+    To be installed:
+    - NodeJS (www.nodejs.org) or "brew nodejs" for mac
+    - MongoDB (www.mongodb.com) or "brew install mongodb" for mac
+
+    Check if installations were successful by using the following commands:
+    - node --version (needs to be 8+)
+    - npm --version (needs to be 5+)
+    - mongod (will start your mongodb instance)
+
+    How to run files? Try by running this file
+
+ */
+
+/*
+
+    1. Print "Hello node" to the stdout
+
+ */
---
+++ _exercises/1-file-system.js
@@ -0,0 +1,30 @@
+/*
+
+    1. List the files in the folder directory (in data). Blocking and Non-blocking.
+
+ */
+
+/*
+
+    2. Read the numbers file (blocking) and remove all empty lines (console.log an array containing the numbers)
+
+ */
+
+/*
+
+    3. Read the numbers file (non-blocking), remove the empty lines, and display the sum
+
+ */
+
+/*
+
+    4. Read the movies.json (try not to use fs), filter the list to display only stanley kubrick films, and write the
+    list to a new file called "kubrick.json" 
+
+ */
+
+/*
+
+    5. Create a function that returns a random name from the names file.
+
+ */
