376e886b90611d2bd0f70cab18de13306913128d
Arne Van Raepenbusch <arne.vanraepenbusch@jstack.eu>
2018-03-29 14:24:52 +0200

    Added some new exercises

---
+++ _exercises/rest/Server.js
@@ -0,0 +1,27 @@
+/*
+
+     Create a REST API that handles movies
+
+    1 Create a Mongoose Model (movie)
+        - name
+        - year
+        - director
+
+    2 Create a Mongoose model (user)
+        - username
+        - password
+
+    3 Create a Mongoose model (rating)
+        - movie_id
+        - user_id
+        - rating (1-10)
+
+    4 Create CRUD (service / repository) for the three entities (try to do it as generic as possible)
+
+    5 Create a route that gets top 5 rated movies of a certain user
+
+    6 Do authentication and create a JSON Web Token
+
+    7 Implement CORS
+
+ */
