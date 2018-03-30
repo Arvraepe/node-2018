e6a2bc07322769c097da832d5547da4b08324cb3
Arne Van Raepenbusch <arne.vanraepenbusch@jstack.eu>
2018-03-30 14:04:06 +0200

    Added some new exercises

---
+++ _exercises/2-modules.js
@@ -0,0 +1,11 @@
+/*
+
+    1. Create a calculator module in the 'test-module' file.
+    - export an add function
+    - export a multiply function
+    - export a divide function
+    - export a subtract function
+
+    require the module here and test the four functions
+
+ */
